6bfc9e485f7fec7e91cf07565d21b751a7155ed6
forlubir <lucio40320@gmail.com>
2022-05-04 13:53:28 +0100

    initial set up

---
+++ lstm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on May 4, 2022
+@author: luciohuang
+"""
+"""
+1. Models
+
+"""
---
+++ pre_processing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on May 4, 2022
+@author: luciohuang
+"""
+"""
+1. pre-processing data
+2. Functions:
+
+pre-avg_returns(df, frequency, start_date, end_date):
+    return pre-avged retrun series
+
+pre-avg_RV(df, frequency, start_date, end_date):
+    retrun pre-avged RV series
+
+"""
